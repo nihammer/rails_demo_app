45a0ae9ffb0a51a977d7da4230c73f62c88a5601
DINHNHAM <dinhnhamnguyen@gmail.com>
2018-02-22 23:20:06 +0000

    fix search UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
